687ffb70f51b3be4fed6fc3f710e85d31a5bdca0
KimMinSu <yaminsu5674@gmail.com>
2022-05-16 09:33:34 +0900

    ADD_myprogram_and_inout_files

---
+++ minsu_program.py
@@ -0,0 +1,34 @@
+import os
+import json
+import yaml
+
+print('Please enter the filename you want to convert with extension :', end=' ')
+name = input()
+filename, file_extension = os.path.splitext(name)
+if file_extension == '.yaml':
+    with open('in.yaml') as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+    with open('out.json', 'w') as f:
+        json.dump(data, f, indent=2)
+    print('Successfully converted!')
+
+elif file_extension == '.yml':
+    with open('in.yml') as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+    with open('out.json', 'w') as f:
+        json.dump(data, f, indent=2)
+    print('Successfully converted!')
+
+elif file_extension == '.json':
+    with open("in.json", "r") as f:
+        data = json.load(f)
+    with open('out.yaml', 'w') as f:
+        yaml.dump(data, f)
+    print('Successfully converted!')
+
+else:
+    with open("in.jsn", "r") as f:
+        data = json.load(f)
+    with open('out.yaml', 'w') as f:
+        yaml.dump(data, f)
+    print('Successfully converted!')
